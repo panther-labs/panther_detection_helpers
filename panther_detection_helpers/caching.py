8e421b70582ad593164675113386d05d0cf91839
Yusuf Akhtar <yusuf.akhtar@panther.com>
2024-03-15 13:37:20 -0500

    Using env var to determine KV store

--- panther_detection_helpers/caching.py
+++ panther_detection_helpers/caching.py
@@ -35,7 +35,9 @@ def kv_table() -> boto3.resource:
         _KV_TABLE = boto3.resource(
             "dynamodb",
             endpoint_url="https://dynamodb" + FIPS_SUFFIX if FIPS_ENABLED else None,
-        ).Table("panther-kv-store")
+        ).Table(os.getenv("KV_STORE_TABLE_NAME", "panther-kv-store"))
+    # FIXME
+    print("KV_TABLE: ", _KV_TABLE)
     return _KV_TABLE
 
 
